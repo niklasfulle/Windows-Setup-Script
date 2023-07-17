ba1a8f6ab929021c79164b1ce36a98ec0f657c15
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-07-18 01:28:38 +0200

    Terminal Config

---
+++ terminal/setup.sh
@@ -0,0 +1,20 @@
+# Powershell
+# install oh-my-posh
+winget install JanDeDobbeleer.OhMyPosh -s winget
+
+New-Item -Path $PROFILE -Type File -Force
+
+notepad $PROFILE
+
+oh-my-posh init pwsh --config 'C:\Users\Niklas\AppData\Local\Programs\oh-my-posh\themes\niklasfulle.omp.json' | Invoke-Expression
+
+. $PROFILE
+
+# WSL 
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/niklas/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
